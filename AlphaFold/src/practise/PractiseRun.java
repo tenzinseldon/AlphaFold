d795e8690135401e68b48e3d98533dd543bbbb2f
Owner <Owner@192.168.2.17>
2020-12-15 20:15:06 -0500

    Practice run

---
+++ AlphaFold/src/practise/PractiseRun.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package practise;
+
+/**
+ * @author Seldon, Kelsang, Sanjana
+ *
+ */
+public class PractiseRun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//HIII
+	}
+
+}
